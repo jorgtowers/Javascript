aade0c4a283db0aab0e402ea42e32f734ded76e3
Jorge L. Torres A <jorgtowers@gmail.com>
2016-01-19 20:12:18 -0430

    Update App.js
    Se integra sortTable.js al objecto App.UI.Tablas.Ordenacion:{}

--- App.js
+++ App.js
@@ -52,11 +52,14 @@
         //Activa el buscador del filterTable.js y sortTable.js
         var filtro = document.getElementById("filtro");
         var tabla = document.getElementById("listado");
-        if (filtro != null)
-	    this.UI.Tablas.Busqueda.Init();	
+        if (filtro != null){
+            this.UI.Tablas.Busqueda.Init();
+            this.UI.Tablas.Ordenacion.Init();
             filtro.onkeyup = function () {
-                self.UI.Tablas.Busqueda.Buscar(filtro,tabla);
+                self.UI.Tablas.Busqueda.Buscar(filtro, tabla);
+                //filterTable(filtro, tabla);
             };
+        }
         //Activa el paginador del dataTables.js
         try{
             $('#listado').dataTable({
@@ -1409,7 +1412,323 @@
                 }
             },
             Ordenacion:{
+                Images:{
+                    Path:"http://www.joostdevalk.nl/code/sortable-table/",
+                    Up:"arrow-up.gif",
+                    Down:"arrow-down.gif",
+                    None:"arrow-none.gif"
+                },                
+                EuropeanDate:true,
+                AlternateRowColors:true,
+                SORT_COLUMN_INDEX:0,
+                THead:false,
+                Compare:function(a,b){
+                    var a = parseFloat(a);
+                    a = (isNaN(a) ? 0 : a);
+                    var b = parseFloat(b);
+                    b = (isNaN(b) ? 0 : b);
+                    return a - b;
+                },
+                Alternate:function(table){
+                    // Take object table and get all it's tbodies.
+                    var tableBodies = table.getElementsByTagName("tbody");
+                    // Loop through these tbodies
+                    for (var i = 0; i < tableBodies.length; i++) {
+                        // Take the tbody, and get all it's rows
+                        var tableRows = tableBodies[i].getElementsByTagName("tr");
+                        // Loop through these rows
+                        // Start at 1 because we want to leave the heading row untouched
+                        for (var j = 0; j < tableRows.length; j++) {
+                            var item=tableRows[j];
+                            // Check if j is even, and apply classes for both possible results
+                            if ((j % 2) == 0) {
+                                if (!(item.className.indexOf('odd') == -1)) {
+                                    item.className = item.className.replace('odd', 'even');
+                                } else {
+                                    if (item.className.indexOf('even') == -1) {
+                                        item.className += " even";
+                                    }
+                                }
+                            } else {
+                                if (!(item.className.indexOf('even') == -1)) {
+                                    item.className = item.className.replace('even', 'odd');
+                                } else {
+                                    if (item.className.indexOf('odd') == -1) {
+                                        item.className += " odd";
+                                    }
+                                }
+                            }
+                        }
+                    }                    
+                },
+                Trim:function(s){
+                    //Retorna un String con sin espacios en blanco innesarios
+                    return s.replace(/^\s+|\s+$/g, "");
+                },
+                CleanNum:function(str){
+                    str = str.replace(new RegExp(/[^-?0-9.]/g), "");
+                    return str;
+                },
+                SortDefault:function(a,b){
+                    var aa = this.InnerText(a.cells[this.SORT_COLUMN_INDEX]);
+                    var bb = this.InnerText(b.cells[this.SORT_COLUMN_INDEX]);
+                    if (aa == bb) {
+                        return 0;
+                    }
+                    if (aa < bb) {
+                        return -1;
+                    }
+                    return 1;
+                },
+                SortDate:function(date){
+                    // y2k notes: two digit years less than 50 are treated as 20XX, greater than 50 are treated as 19XX
+                    var dt = "00000000";
+                    if (date.length == 11) {
+                        var mtstr = date.substr(3, 3);
+                        mtstr = mtstr.toLowerCase();
+                        switch (mtstr) {
+                            case "jan": var mt = "01"; break;
+                            case "feb": var mt = "02"; break;
+                            case "mar": var mt = "03"; break;
+                            case "apr": var mt = "04"; break;
+                            case "may": var mt = "05"; break;
+                            case "jun": var mt = "06"; break;
+                            case "jul": var mt = "07"; break;
+                            case "aug": var mt = "08"; break;
+                            case "sep": var mt = "09"; break;
+                            case "oct": var mt = "10"; break;
+                            case "nov": var mt = "11"; break;
+                            case "dec": var mt = "12"; break;
+                                // default: var mt = "00";
+                        }
+                        dt = date.substr(7, 4) + mt + date.substr(0, 2);
+                        return dt;
+                    } else if (date.length == 10) {
+                        if (this.EuropeanDate == false) {
+                            dt = date.substr(6, 4) + date.substr(0, 2) + date.substr(3, 2);
+                            return dt;
+                        } else {
+                            dt = date.substr(6, 4) + date.substr(3, 2) + date.substr(0, 2);
+                            return dt;
+                        }
+                    } else if (date.length == 8) {
+                        yr = date.substr(6, 2);
+                        if (parseInt(yr) < 50) {
+                            yr = '20' + yr;
+                        } else {
+                            yr = '19' + yr;
+                        }
+                        if (this.EuropeanDate == true) {
+                            dt = yr + date.substr(3, 2) + date.substr(0, 2);
+                            return dt;
+                        } else {
+                            dt = yr + date.substr(0, 2) + date.substr(3, 2);
+                            return dt;
+                        }
+                    }
+                    return dt;
+                },
+                Parent:function(el,pTagName){
+                    if (el == null) {
+                        return null;
+                    } else if (el.nodeType == 1 && el.tagName.toLowerCase() == pTagName.toLowerCase()) {
+                        return el;
+                    } else {
+                        return this.Parent(el.parentNode, pTagName);
+                    }
+                },
+                ResortTable:function(lnk,clid){
+                    var self=this;
+                    var span;
+                    for (var ci = 0; ci < lnk.childNodes.length; ci++) {
+                        if (lnk.childNodes[ci].tagName && lnk.childNodes[ci].tagName.toLowerCase() == 'span') {
+                            span = lnk.childNodes[ci];
+                        }
+                    }
+                    var spantext = this.InnerText(span);
+                    var td = lnk.parentNode;
+                    var column = clid || td.cellIndex;
+                    var t = this.Parent(td, 'TABLE');
+                    // Work out a type for the column
+                    if (t.rows.length <= 1) return;
+                    var itm = "";
+                    var i = 0;
+                    while (itm == "" && i < t.tBodies[0].rows.length) {
+                        var itm = this.InnerText(t.tBodies[0].rows[i].cells[column]);
+                        itm = this.Trim(itm);
+                        if (itm.substr(0, 4) == "<!--" || itm.length == 0) {
+                            itm = "";
+                        }
+                        i++;
+                    }
+                    if (itm == "") return;
+                    var oCASEINSENTITIVE=0,oDATE=1,oNUMERIC=2;
+                    var sortfn = oCASEINSENTITIVE;
+                    if (itm.match(/^\d\d[\/\.-][a-zA-z][a-zA-Z][a-zA-Z][\/\.-]\d\d\d\d$/)) {sortfn = oDATE;}
+                    if (itm.match(/^\d\d[\/\.-]\d\d[\/\.-]\d\d\d{2}?$/)) {sortfn = oDATE;}
+                    if (itm.match(/^-?[£$€Û¢´]\d/)) {sortfn = oNUMERIC;}
+                    if (itm.match(/^-?(\d+[,\.]?)+(E[-+][\d]+)?%?$/)) {sortfn = oNUMERIC;}
+                    this.SORT_COLUMN_INDEX = column;
+                    var firstRow = new Array();
+                    var newRows = new Array();
+                    for (var k = 0; k < t.tBodies.length; k++) {
+                        for (i = 0; i < t.tBodies[k].rows[0].length; i++) {
+                            firstRow[i] = t.tBodies[k].rows[0][i];
+                        }
+                    }
+                    for (var k = 0; k < t.tBodies.length; k++) {
+                        if (!this.THead) {
+                            // Skip the first row
+                            for (var j = 1; j < t.tBodies[k].rows.length; j++) {
+                                newRows[j - 1] = t.tBodies[k].rows[j];
+                            }
+                        } else {
+                            // Do NOT skip the first row
+                            for (var j = 0; j < t.tBodies[k].rows.length; j++) {
+                                newRows[j] = t.tBodies[k].rows[j];
+                            }
+                        }
+                    }
 
+                   switch (sortfn) {
+                        case oNUMERIC:{
+                            newRows.sort(function(a,b){
+                                var aa = self.InnerText(a.cells[self.SORT_COLUMN_INDEX]);
+                                aa = self.CleanNum(aa);
+                                var bb = self.InnerText(b.cells[self.SORT_COLUMN_INDEX]);
+                                bb = self.CleanNum(bb);
+                                return self.Compare(aa, bb);
+                            });
+                            break;
+                        } 
+                        case oDATE:{
+                            newRows.sort(function (a, b) {
+                                var dt1 = self.SortDate(self.InnerText(a.cells[self.SORT_COLUMN_INDEX]));
+                                var dt2 = self.SortDate(self.InnerText(b.cells[self.SORT_COLUMN_INDEX]));
+                                if (dt1 == dt2) {
+                                    return 0;
+                                }
+                                if (dt1 < dt2) {
+                                    return -1;
+                                }
+                                return 1;
+                            });
+                            break;
+                        }
+                        default:{
+                            newRows.sort(function(a,b){
+                                var aa = self.InnerText(a.cells[self.SORT_COLUMN_INDEX]).toLowerCase();
+                                var bb = self.InnerText(b.cells[self.SORT_COLUMN_INDEX]).toLowerCase();
+                                if (aa == bb) {
+                                    return 0;
+                                }
+                                if (aa < bb) {
+                                    return -1;
+                                }
+                                return 1;
+                            });    
+                        }
+        
+                    }
+                    
+                    if (span.getAttribute("sortdir") == 'down') {
+                        this.ARROW = '&nbsp;&nbsp;<b class="fa fa-caret-down"></b>';
+                        //ARROW = '&nbsp;&nbsp;<img src="' + image_path + image_down + '" alt="&darr;"/>';
+                        newRows.reverse();
+                        span.setAttribute('sortdir', 'up');
+                    } else {
+                        this.ARROW = '&nbsp;&nbsp;<b class="fa fa-caret-up"></b>';
+                        //ARROW = '&nbsp;&nbsp;<img src="' + image_path + image_up + '" alt="&uarr;"/>';
+                        span.setAttribute('sortdir', 'down');
+                    }
+                    // We appendChild rows that already exist to the tbody, so it moves them rather than creating new ones
+                    // don't do sortbottom rows
+                    for (var i = 0; i < newRows.length; i++) {
+                        if (!newRows[i].className || (newRows[i].className && (newRows[i].className.indexOf('sortbottom') == -1))) {
+                            t.tBodies[0].appendChild(newRows[i]);
+                        }
+                    }
+                    // do sortbottom rows only
+                    for (var i = 0; i < newRows.length; i++) {
+                        if (newRows[i].className && (newRows[i].className.indexOf('sortbottom') != -1))
+                            t.tBodies[0].appendChild(newRows[i]);
+                    }
+                    // Delete any other arrows there may be showing
+                    var allspans = document.getElementsByTagName("span");
+                    for (var ci = 0; ci < allspans.length; ci++) {
+                        if (allspans[ci].className == 'sortarrow') {
+                            if (this.Parent(allspans[ci], "table") == this.Parent(lnk, "table")) { // in the same table as us?
+                                allspans[ci].innerHTML = '&nbsp;&nbsp;';
+                                //allspans[ci].innerHTML = '&nbsp;&nbsp;<img src="' + image_path + image_none + '" alt="&darr;"/>';
+                            }
+                        }
+                    }
+                    span.innerHTML = this.ARROW;
+                    this.Alternate(t);
+                },
+                InnerText:function(el){
+                    if (typeof el == "string") {return el;}
+                    if (typeof el == "undefined") { return el };
+                    if (el.innerText) {return el.innerText;  }
+                    var str = "";
+                    var cs = el.childNodes;
+                    var l = cs.length;
+                    for (var i = 0; i < l; i++) {
+                        switch (cs[i].nodeType) {
+                            case 1: //ELEMENT_NODE
+                                str += this.InnerText(cs[i]);
+                                break;
+                            case 3: //TEXT_NODE
+                                str += cs[i].nodeValue;
+                                break;
+                        }
+                    }
+                    return str;
+                },
+                MakeSortable:function(t){
+                    if (t.rows && t.rows.length > 0) {
+                        if (t.tHead && t.tHead.rows.length > 0) {
+                            var firstRow = t.tHead.rows[t.tHead.rows.length - 1];
+                            this.THead = true;
+                        } else {
+                            var firstRow = t.rows[0];
+                        }
+                    }
+                    if (!firstRow) {return;}
+                    // We have a first row: assume it's the header, and make its contents clickable links
+                    for (var i = 0; i < firstRow.cells.length; i++) {
+                        var cell = firstRow.cells[i];
+                        var txt = this.InnerText(cell);
+                        if (cell.className != "unsortable" && cell.className.indexOf("unsortable") == -1) {
+                            //cell.innerHTML = '<a href="#" class="sortheader" onclick="App.UI.Tablas.Ordenacion.ResortTable(this, ' + i + ');return false;">' + txt + '<span class="sortarrow">&nbsp;&nbsp;</span></a>';
+                            cell.innerHTML = '<a href="#" class="sortheader" sort="'+i+'">' + txt + '<span class="sortarrow">&nbsp;&nbsp;</span></a>';
+                            //cell.innerHTML = '<a href="#" class="sortheader" onclick="ts_resortTable(this, ' + i + ');return false;">' + txt + '<span class="sortarrow">&nbsp;&nbsp;<img src="' + image_path + image_none + '" alt="&darr;"/></span></a>';
+                        }
+                    }
+                    if (this.AlternateRowColors) {
+                        this.Alternate(t);
+                    }
+                },
+                Init:function(){
+                    var self=this;
+                    if (!document.getElementsByTagName) {return;}
+                    var tbls = document.getElementsByTagName("table");
+                    for (var ti = 0; ti < tbls.length; ti++) {
+                        var thisTbl = tbls[ti];
+                        if (((' ' + thisTbl.className + ' ').indexOf("sortable") != -1) && (thisTbl.id)) {
+                            this.MakeSortable(thisTbl);
+                        }
+                    }
+                    var trSort=document.getElementsByClassName("sortheader");
+                    if(trSort!==null){
+                        for (var i = 0; i < trSort.length; i++) {
+                            trSort[i].onclick=function(){
+                                self.ResortTable(this,this.getAttribute("sort"));
+                                return false;
+                            }
+                        }
+                    }
+                }
             }
         }
     };
