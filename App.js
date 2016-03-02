40ecdc6a216b9ecc038e6d8acc1726ec54f3a0a7
Jorge L. Torres A <jorgtowers@gmail.com>
2016-03-02 08:18:52 -0430

    Update App.js

--- App.js
+++ App.js
@@ -1746,28 +1746,28 @@
                 return arr;             
              };
              String.prototype.ToTitleCase = function () {
-                  var i, j, str, lowers, uppers;
-                  str = this.replace(/([^\W_]+[^\s-]*) */g, function (txt) {
-                      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            var i, j, str, lowers, uppers;
+            str = this.replace(/([^\W_]+[^\s-]*) */g, function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+
+            // Certain minor words should be left lowercase unless 
+            // they are the first or last words in the string
+            lowers = ['el', 'en', 'lo', 'con', 'un', 'la', 'los', 'de', 'desde', 'hasta', 'De', 'En', 'La', 'Un'];
+            for (i = 0, j = lowers.length; i < j; i++)
+                str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'),
+                  function (txt) {
+                      return txt.toLowerCase();
                   });
-      
-                  // Certain minor words should be left lowercase unless 
-                  // they are the first or last words in the string
-                  lowers = ['el', 'en', 'lo', 'con', 'un', 'la', 'los', 'de', 'desde', 'hasta'];
-                  for (i = 0, j = lowers.length; i < j; i++)
-                      str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'),
-                        function (txt) {
-                            return txt.toLowerCase();
-                        });
-      
-                  // Certain words such as initialisms or acronyms should be left uppercase
-                  uppers = ['Id', 'Tv'];
-                  for (i = 0, j = uppers.length; i < j; i++)
-                      str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'),
-                        uppers[i].toUpperCase());
-      
-                  return str;
-              }
+
+            // Certain words such as initialisms or acronyms should be left uppercase
+            uppers = ['Id', 'Tv'];
+            for (i = 0, j = uppers.length; i < j; i++)
+                str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'),
+                  uppers[i].toUpperCase());
+
+            return str;
+        }
              Array.prototype.Radios = function () {
                 var arr = this;
                 var _ = {
